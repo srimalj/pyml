1acefaa16a4ac9f6876479c95880bfcc5b156d91
Srimal Jayawardena <srimalj@gmail.com>
2016-02-17 10:47:15 +1000

    Initial class skeleton for ensemble.

---
+++ ensemble.py
@@ -0,0 +1,19 @@
+# -*- coding: utf8
+
+"""
+The :mod:`ensemble` module implements ensemble of classifiers
+"""
+
+
+import numpy as np
+from scipy.linalg import pinv2
+
+class Ensemble:
+    'Ensemble of learners'
+
+    def fit(self):
+        return
+    
+    def predict_proba(self):
+        return
+    
