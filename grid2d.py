f8ebab97919b7e31918cb8d6fe7305aec18a5c8f
Srimal Jayawardena <srimalj@gmail.com>
2016-06-21 16:48:24 +1000

    Grid2d class with fit method to fit estimator models to each cell.

--- grid2d.py
+++ grid2d.py
@@ -1,5 +1,8 @@
 # Partitions features to a 2D grid and trains a model on each grid cell
+
 import numpy as np
+from sklearn.externals import joblib
+import sklearn.base
 
 ################################################################################
 def getCellData(X, y, min0, max0, min1, max1):
@@ -16,10 +19,25 @@ def getCellData(X, y, min0, max0, min1, max1):
 
     return np.array(Xcell), np.array(ycell)
 
-# class grid2d():
-#     def __grid2d__(X, xlims, ylims, estimator):
-        
-        
-#     def fit()
-
+################################################################################
+class Grid2d():
     
+    def __init__(self, limits0, limits1, estimator):
+        self.limits0 = limits0
+        self.limits1 = limits1
+        self.estimator = estimator
+        
+    def fit(self, X, y):
+
+        xs = self.limits0
+        ys = self.limits1
+        
+        for ix in range(0, len(xs)-1):
+            for iy in range(0, len(ys)-1):
+                
+                Xcell, ycell  = getCellData(X, y, xs[ix], xs[ix + 1], ys[iy], ys[iy + 1])
+                print("Cell (%d,%d): (%f-%f, %f-%f) samples = %d classes = %d" %
+                      (ix, iy, xs[ix], xs[ix + 1], ys[iy], ys[iy + 1], Xcell.shape[0], len(np.unique(ycell))))
+                cell = sklearn.base.clone(self.estimator)
+                cell.fit(Xcell, ycell)
+                joblib.dump(cell, "cell_%d_%d.pkl" % (ix, iy), compress=True)
