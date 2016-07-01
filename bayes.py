4a311be66df409283806fac9c3d3e3a831d0ae7f
Srimal Jayawardena <srimalj@gmail.com>
2016-07-01 13:55:05 +1000

    Initial code for bayes classifier using KDE.

--- bayes.py
+++ bayes.py
@@ -43,16 +43,28 @@ class BayesClassifierKDE():
         # Prior distributions
         self.prior = np.log(priorDistribution(y))
 
+        
     def predict(self, X):
-        return
+        return self.predictk(X, 1).reshape(-1, 1)
 
+    
     def predict_proba(self, X):
         out = np.zeros(X.shape[0], len(self.classes_))
-        for r in xrange(0, X.shape[0]):
-            for c in xrange(0, len(self.classes_)):
-                out[r, c] = self.cpdf[c] + self.prior[c]
+        for row in out:
+            row = self.posteriors(row)
         return out
 
-    def predictk(self, X):
-        return
+    
+    def predictk(self, X, k):
+        out = np.zeros(X.shape[0], k)
+        for row in xrange(0, X.shape[0]):
+            bestIndices = self.classescommon.largestK(self.posteriors(X[row, :]))
+            out[row, :] = self.classes_[bestIndices]
+        return out
 
+    
+    def posteriors(self, x):
+        values = np.zeros(len(x))
+        for k in xrange(0, len(self.classes_)):
+            values[k] = self.cpdf[k].score(x.reshape(1, -1)) + self.prior[k]
+        return values
